afb42cb478876d29bbd643da59d0f0c063f1c000
EmmanuelSantiago <66569479+EmmanuelSantiago@users.noreply.github.com>
2021-01-27 19:57:27 -0500

    update error main

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 let af = true;
 let ne = false;
 
-console.log(`%cOperador and '&&', "color:blue;`);
+console.log(`%cOperador and '&&', "color:blue";`);
 console.log(`${ne} and ${ne} = ${ne && ne}`);
 console.warn(`${af} and ${ne} = ${af && ne}`);
 console.log(`${ne} and ${af} = ${ne && af}`);
 console.warn(`${af} and ${af} = ${af && af}`);
 
 //Operador OR ||
-console.log(`%cOperador or '||', "color:yellow;`);
+console.log(`%cOperador or '||', "color:yellow";`);
 console.log(`${ne} or ${ne} = ${ne || ne}`);
 console.warn(`${af} or ${ne} = ${af || ne}`);
 console.log(`${ne} or ${af} = ${ne || af}`);
 console.warn(`${af} or ${af} = ${af || af}`);
 
 //Operador NOT !
-console.log(`%cOperador not '!', "color:green;`);
+console.log(`%cOperador not '!', "color:green";`);
 console.log(`${ne} = ${!ne}`);
 console.warn(`${af} = ${!af}`);
 
